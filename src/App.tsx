ebe41d56eea7afdf58c5891b94a0074d2e51d637
snax10 <carlos_santos0702@hotmail.com>
2024-11-10 07:21:20 -0300

    feat: adicionando biome js

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,64 @@
-import { Refine } from "@refinedev/core";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import { Refine } from '@refinedev/core';
+import { RefineKbar, RefineKbarProvider } from '@refinedev/kbar';
 
-import {
-  notificationProvider,
-  RefineSnackbarProvider,
-} from "@refinedev/mui";
+import { RefineSnackbarProvider, notificationProvider } from '@refinedev/mui';
 
-import CssBaseline from "@mui/material/CssBaseline";
-import GlobalStyles from "@mui/material/GlobalStyles";
+import CssBaseline from '@mui/material/CssBaseline';
+import GlobalStyles from '@mui/material/GlobalStyles';
 import routerBindings, {
-  DocumentTitleHandler,
-  UnsavedChangesNotifier,
-} from "@refinedev/react-router-v6";
-import { dataProvider, liveProvider } from "@refinedev/supabase";
-import { BrowserRouter } from "react-router-dom";
-import { ColorModeContextProvider } from "./contexts/color-mode";
-import { supabaseClient } from "./utils/supabaseClient";
-import { AppRoutes } from "./routes";
+	DocumentTitleHandler,
+	UnsavedChangesNotifier,
+} from '@refinedev/react-router-v6';
+import { dataProvider, liveProvider } from '@refinedev/supabase';
+import { BrowserRouter } from 'react-router-dom';
+import { ColorModeContextProvider } from './contexts/color-mode';
+import { AppRoutes } from './routes';
+import { supabaseClient } from './utils/supabaseClient';
 
 export function App() {
-  return (
-    <BrowserRouter>
-      <RefineKbarProvider>
-        <ColorModeContextProvider>
-          <CssBaseline />
-          <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
-          <RefineSnackbarProvider>
-            <Refine
-              dataProvider={dataProvider(supabaseClient)}
-              liveProvider={liveProvider(supabaseClient)}
-              routerProvider={routerBindings}
-              notificationProvider={notificationProvider}
-              resources={[
-                {
-                  name: "appointments",
-                  list: "/appointments",
-                  create: "/appointments/create",
-                  edit: "/appointments/edit/:id",
-                  show: "/appointments/show/:id",
-                  meta: {
-                    canDelete: true,
-                    label: 'Evolução de RCP',
-                  },
-                },
-              ]}
-              options={{
-                syncWithLocation: true,
-                warnWhenUnsavedChanges: true,
-                useNewQueryKeys: true,
-                title: {
-                  text: 'Integra',
-                }
-              }}
-            >
-              <AppRoutes />
+	return (
+		<BrowserRouter>
+			<RefineKbarProvider>
+				<ColorModeContextProvider>
+					<CssBaseline />
+					<GlobalStyles styles={{ html: { WebkitFontSmoothing: 'auto' } }} />
+					<RefineSnackbarProvider>
+						<Refine
+							dataProvider={dataProvider(supabaseClient)}
+							liveProvider={liveProvider(supabaseClient)}
+							routerProvider={routerBindings}
+							notificationProvider={notificationProvider}
+							resources={[
+								{
+									name: 'appointments',
+									list: '/appointments',
+									create: '/appointments/create',
+									edit: '/appointments/edit/:id',
+									show: '/appointments/show/:id',
+									meta: {
+										canDelete: true,
+										label: 'Evolução de RCP',
+									},
+								},
+							]}
+							options={{
+								syncWithLocation: true,
+								warnWhenUnsavedChanges: true,
+								useNewQueryKeys: true,
+								title: {
+									text: 'Integra',
+								},
+							}}
+						>
+							<AppRoutes />
 
-              <RefineKbar />
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
-            </Refine>
-          </RefineSnackbarProvider>
-        </ColorModeContextProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
-  );
+							<RefineKbar />
+							<UnsavedChangesNotifier />
+							<DocumentTitleHandler />
+						</Refine>
+					</RefineSnackbarProvider>
+				</ColorModeContextProvider>
+			</RefineKbarProvider>
+		</BrowserRouter>
+	);
 }
