7a5466b1c1aa92525688c93fe545341c662879f7
snax10 <carlos_santos0702@hotmail.com>
2025-09-02 17:45:20 -0300

    Revert "Merge branch 'feat/supabase-integration' into develop"
    This reverts commit 9214faf62d3aa6020370381f519c39f86d25ff13, reversing
    changes made to 2d0257a2a047c3392d39b9898eb4ec3411d7c659.

--- src/store/types/appointments.store.types.ts
+++
@@ -1,7 +0,0 @@
-import { AppointmentData } from "@/types/appointment";
-
-export interface AppointmentStore {
-  appointmentList: AppointmentData[];
-
-  setAppointmentList: (items: AppointmentData[]) => void;
-}
--- src/utils/formatedData.ts
+++
@@ -1,7 +0,0 @@
-export const formatDate = (date: string) => {
-    return new Date(date).toLocaleDateString('pt-BR');
-  };
-
- export const formatTime = (date: string) => {
-    return new Date(date).toLocaleTimeString('pt-BR', { hour: '2-digit', minute: '2-digit' });
-  };
